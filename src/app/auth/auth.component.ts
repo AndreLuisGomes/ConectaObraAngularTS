a2e5eca70cdebb789857cb7fbb3ae6237f470578
AndreLuisGomes <andreluisgomes09@gmail.com>
2026-02-17 17:05:16 -0400

    fix: corrigindo erros de lógica e UX

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, UrlTree } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { ApiError } from '../models/errors/error';
 
 @Component({
   selector: 'app-auth',
@@ -13,8 +14,9 @@ import { AuthService } from '../services/auth.service';
 export class AuthComponent {
 
   btnLogar = signal<boolean>(true);
-  senhaValida = signal(true);
-  usuarioErro = signal<number>(0);
+  // botaoLogar = computed(() => this.btnLogar());
+  senhaValida = signal<boolean>(true);
+  mensagemErro = signal<string>("");
   loginForm!: FormGroup;
   mostrarSenhaBool: boolean = false;
   authService: AuthService = inject(AuthService);
@@ -22,39 +24,34 @@ export class AuthComponent {
 
   constructor() {
     this.loginForm = new FormGroup({
-      nome: new FormControl('', [Validators.required]),
-      senha: new FormControl('', [Validators.required])
+      nome: new FormControl('', Validators.required),
+      senha: new FormControl('', Validators.required)
     })
   }
 
-  logar(): null | UrlTree {
+  logar() {
     this.loginForm.markAllAsTouched();
     console.log('Entrando no logar');
-
-    if (!!this.loginForm.valid) {
+    this.btnLogar.set(true);
+    if (this.loginForm.valid) {
       console.log('Entrando no If')
-      this.btnLogar.set(false);
       this.authService.logar(this.loginForm.value).subscribe({
         next: () => {
           console.log('Entrando no next')
-
-          this.btnLogar.set(true)
-          return this.router.createUrlTree(['/clientes'])
+          this.router.navigate(['/suportes']);
         },
-        error: (err) => {
+        error: (err : ApiError) => {
+          this.mensagemErro.set(err.error);
           console.error(err);
-          this.btnLogar.set(true);
         }
       })
     }
-    this.usuarioErro.set(1);
-    console.log('Deu errado!')
-    return null;
+    this.btnLogar.set(true);
   }
 
   campoEstaValido(campoNome: string): boolean {
     const campo = this.loginForm.get(campoNome);
-    return !!campo && campo.valid && (campo?.touched || campo?.dirty)
+    return !!campo && campo?.valid && (campo?.touched || campo?.dirty)
   }
 
   mostrarSenha() {
