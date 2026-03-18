5797269a0d2e8c4784d48b21846225b95eebd829
AndreLuisGomes <andreluisgomes09@gmail.com>
2026-03-18 19:16:21 -0400

    refactor: adicionando e corrigindo estruturas da aplicação

--- src/app/environments/environment.development.ts
+++ src/app/environments/environment.development.ts
@@ -1,6 +1,19 @@
 export const environment = {
+
+    // API URLs \\
+
     apiUrl : 'http://localhost:8080',
-    refreshTokenUrl : 'http://localhost:8080/refresh-token',
+    apiLoginUrl : 'http://localhost:8080/auth/logar',
+    apiRegistrarUrl : 'htt:localhost:8080/auth/registrar',
+    apiRefreshTokenUrl : 'http://localhost:8080/auth/refresh-token',
+    
+    // Angular URLs \\
+    
+    fazerLoginUrl : '/auth/fazer-login',
+    suportesUrl : '/suportes',
+    guiasUrl : '/guias',
+    clientesUrl : '/clientes',
+    spoolsUrl : '/spools',
 
     // Local Storage Consts \\
 
