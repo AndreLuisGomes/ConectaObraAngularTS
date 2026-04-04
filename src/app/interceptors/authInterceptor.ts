ded6673e9a99c6cf706199f704306a8a7e84e6de
AndreLuisGomes <andreluisgomes09@gmail.com>
2026-04-03 21:22:35 -0400

    fix(auth): corrigindo o bug de não fazer o login automaticamente

--- src/app/interceptors/authInterceptor.ts
+++ src/app/interceptors/authInterceptor.ts
@@ -4,7 +4,7 @@ import { AuthService } from '../services/auth.service';
 import { Inject, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthResponse } from '../models/authResponse';
-import { catchError, Observable, tap, throwError } from 'rxjs';
+import { catchError, Observable, switchMap, tap, throwError } from 'rxjs';
 import { environment } from '../environments/environment.development';
 import { ApiError } from '../models/errors/error';
 
@@ -13,18 +13,18 @@ export class AuthInterceptor implements HttpInterceptor {
     private quantTentativas = 0;
 
     intercept(req: HttpRequest<any>, next: HttpHandler, router = inject(Router), authService = inject(AuthService)): Observable<HttpEvent<any>> {
-        
+
         console.log("AuthInterceptor -> intercept() : Passando pelo AuthInterceptor");
-        
+
         if (
             req.url == environment.apiLoginUrl ||
             req.url == environment.apiRefreshTokenUrl ||
             req.url == environment.apiRefreshTokenUrl
-        ) { 
+        ) {
             console.log("AuthInterceptor -> intercept() : A requisição é de login ou refreshToken")
             return next.handle(req);
         }
-        
+
         let usuario = authService.obterUsuarioDoStorage();
 
         if (!usuario?.accessToken || !usuario?.refreshToken) {
@@ -33,6 +33,7 @@ export class AuthInterceptor implements HttpInterceptor {
             return next.handle(req);
         }
         console.log('AuthInterceptor -> intercept() : Não passou pelo if, não é uma requisição de autenticação')
+        console.log(req.headers)
         let novaReq = req.clone({
             setHeaders: {
                 'Authorization': `Bearer ${authService.obterUsuarioDoStorage()?.accessToken}`
@@ -40,7 +41,7 @@ export class AuthInterceptor implements HttpInterceptor {
         })
 
         return next.handle(novaReq).pipe(
-            catchError((err : ApiError) => {
+            catchError((err: ApiError) => {
                 this.quantTentativas++;
 
                 if (this.quantTentativas > 3) {
@@ -50,33 +51,42 @@ export class AuthInterceptor implements HttpInterceptor {
                 }
 
                 if (err.status === 401) {
-                    this.lidarCom401(novaReq, next, authService, usuario);
+                    return this.lidarCom401(novaReq, next, authService);
                 }
-                return throwError(() => { err });
+                return throwError(() => err);
             })
         )
     }
 
-    lidarCom401(req: HttpRequest<any>, next: HttpHandler, authService: AuthService, usuario : AuthResponse) {
+    lidarCom401(req: HttpRequest<any>, next: HttpHandler, authService: AuthService): Observable<HttpEvent<any>> {
+
         console.log("AuthInterceptor -> lidarCom401() : Passando pelo lidar com 401");
-        authService.refreshToken(authService.obterUsuarioDoStorage()?.refreshToken!)
-        .pipe(catchError((err : ApiError) => {
-            console.log("AuthInterceptor -> lidarCom401() : Deu erro no refreshToken");
-            if(err.status == 401){
-                authService.deslogar();
-            } 
-            return throwError( () => err)
-        }))
-        .subscribe({
-            next: (refreshToken) => {
-                console.log("AuthInterceptor -> lidarCom401 : Definindo novos tokens no Storage e enviando requisição com headers válidos");
-                authService.definirNovosTokens(refreshToken.accessToken, refreshToken.refreshToken);
-                next.handle(req.clone({
+
+        return authService.refreshToken(authService.obterUsuarioDoStorage()?.refreshToken!)
+            .pipe(switchMap((response) => {
+
+                console.log('Chamando authService.refreshToken');
+
+                authService.definirNovosTokens(response.accessToken, response.refreshToken);
+                const novaReq = next.handle(req.clone({
                     setHeaders: {
                         'Authorization': `Bearer ${authService.obterUsuarioDoStorage()?.accessToken}`
                     }
-                }))
-            }
-        })
+                }
+                ))
+
+                console.log('Enviando requisição com tokens válidos!')
+                return novaReq;
+
+            }), catchError((err: ApiError) => {
+                console.log('Deu erro!');
+                console.log('RefreshToken já está inválido!');
+                if (err.status === 401) {
+                    authService.deslogar();
+                }else{
+                    console.log('Aconteceu outro tipo de erro!');
+                }
+                return throwError(() => err);
+            }))
     }
 }
